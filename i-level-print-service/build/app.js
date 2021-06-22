33a718befae7476492df1d2e8cb8c872fe550f9b
Samuel Guard <sg2268@bath.ac.uk>
2021-06-22 17:00:48 +0100

    backend now gets printers and matches them to their preferences

--- i-level-print-service/build/app.js
+++ i-level-print-service/build/app.js
@@ -1,5 +1,6 @@
 const express = require('express');//Used for routing http requests
 const fs = require("fs")
+const printer = require("./printer")
 
 const app = express();
 const bodyparse = require("body-parser");
@@ -7,7 +8,11 @@ const bodyparse = require("body-parser");
 app.use(express.json());
 //app.use(express.static("public"));
 
-app.use("/", function (req, res, next) {
+app.get("/printers", (req, res, next) => {
+    res.send(JSON.stringify(printer.getPrinters()));
+});
+
+app.use("/", (req, res, next) => {
     fs.readFile(process.cwd() + "/assets/frontend/index.html", "utf8", function (err, data) {
         if (err) {
             res.send("<!DOCTYPE html><html>Error 500: File not found!</html>");
---
+++ i-level-print-service/build/printer.js
@@ -0,0 +1,39 @@
+const printer = require("printer")
+const fs = require("fs");
+let config = JSON.parse(fs.readFileSync(process.cwd + "/../printer-config.json", "utf-8"));
+console.log(config)
+
+function getPrinterConfig(name){
+    return config.find(x => x.name == name);
+}
+
+function saveConfig(){
+    fs.writeFileSync(process.cwd + "/../printer-config.json", JSON.stringify(config));
+}
+
+function getPrinters(){
+    let printerData = []
+    changedConfig = false;
+    printer.getPrinters().forEach(el => {
+        const con = getPrinterConfig(el.name);
+        if(con != undefined){
+            el.config = con;
+            printerData.push(el);
+        } else {
+            const newCon = {name: el.name, enabled: true, displayName: el.name }
+            config.push(newCon)
+            el.config = newCon;
+            printerData.push(el);
+            changedConfig = true;
+        }
+    });
+
+    if(changedConfig){
+        saveConfig();
+        console.log("Saved config");
+    }
+    
+    return printerData
+}
+
+module.exports.getPrinters = getPrinters;
