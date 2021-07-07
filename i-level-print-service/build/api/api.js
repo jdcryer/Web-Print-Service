7fc3c8eb310079d68fc56e9349bad01ba9902712
Samuel Guard <sg2268@bath.ac.uk>
2021-07-07 11:37:35 +0100

    added api file

---
+++ i-level-print-service/build/api/api.js
@@ -0,0 +1,7 @@
+const request = require("request");
+
+
+request.get("http://worldtimeapi.org/api/ip", function(err, resp, body) {
+    console.log(`API: THE DATE/TIME IS ${JSON.parse(body).utc_datetime}`);
+})
+
--- i-level-print-service/build/server.js
+++ i-level-print-service/build/server.js
@@ -4,4 +4,6 @@ const app = require("./app")
 let server = http.createServer(app);
 
 server.listen(3000);
+
+const api = require("./api/api");
 //console.log("listening on port 3000");
