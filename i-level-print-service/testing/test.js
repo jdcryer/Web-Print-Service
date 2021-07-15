18b95a34562d0e37e8cde8aef1fdf02bfd852c23
Samuel Guard <sg2268@bath.ac.uk>
2021-07-15 17:44:18 +0100

    ADDED TESTING

--- i-level-print-service/testing/test.js
+++ i-level-print-service/testing/test.js
@@ -1,8 +1,42 @@
-const expect = require("chai").expect;
+const chai = require("chai");
+const chaiAsPromised = require('chai-as-promised')
+const expect = chai.expect;
 const label = require("../build/modules/label_constructor");
+const fs = require("fs");
 
-describe("test", () => {
-        it("testit", () => {
-                expect(1).to.equal(1);
-        })
+chai.use(chaiAsPromised)
+
+function getTestImage(url, data = "") {
+  return new Promise((resolve, reject) => {
+    var options = {
+      encoding: null,
+      formData: { file: data },
+      // omit this line to get PNG images back
+      headers: { Accept: "image/png" },
+      // adjust print density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary
+      url: url,
+    };
+
+    request.post(options, function (err, resp, body) {
+      if (err) {
+        reject(err);
+      }
+      resolve({ link: url, data: body });
+    });
+  });
+}
+
+describe("Module testing", () => {
+  it("Label constructor", () => {
+    testDataJob = fs.readFileSync(__dirname + "/job.json", "utf-8");
+    testDataItem = fs.readFileSync(__dirname + "/items.json", "utf-8");
+    testDataRes = fs.readFileSync(__dirname + "/result_zpl.txt", "utf-8");
+    return expect(
+        label.build(
+        JSON.parse(testDataJob),
+        JSON.parse(testDataItem).printItem[0]
+      )
+    ).to.eventually.equal(testDataRes);
+  });
 });
+//.should.eventually.equal(testDataRes);
