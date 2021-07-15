71724f23853709e1968ac8f0de8a2419051316ba
Samuel Guard <sg2268@bath.ac.uk>
2021-07-15 15:12:15 +0100

    added testing integration

---
+++ i-level-print-service/testing/test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+const label = require("../build/modules/label_constructor");
+
+describe("test", () => {
+        it("testit", () => {
+                expect(1).to.equal(1);
+        })
+});
