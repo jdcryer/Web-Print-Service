a854dc63f576277429d26752f35a63d2a73c19c2
Samuel Guard <sg2268@bath.ac.uk>
2021-08-23 17:04:51 +0100

    added service handling

--- print-app/src/pages/MainPage.js
+++ print-app/src/pages/MainPage.js
@@ -2,16 +2,27 @@ import React from "react";
 const electron = window.require("electron");
 const remote = electron.remote;
 const { dialog } = remote;
+const { service } = require("../serviceHandler");
 
 function MainPage() {
   return (
-    <button
-      onClick={() => {
-        dialog.showErrorBox("Error Box", "Fatal Error");
-      }}
-    >
-      Show Error Box
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          dialog.showErrorBox("Error Box", "Fatal Error");
+        }}
+      >
+        Show Error Box
+      </button>
+
+      <button
+        onClick={() => {
+          service("install");
+        }}
+      >
+        Install Service
+      </button>
+    </div>
   );
 }
 
