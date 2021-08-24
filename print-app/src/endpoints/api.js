d0aaab9a9fbd4366264be1e0c7bd08d80fa555f3
Samuel Guard <sg2268@bath.ac.uk>
2021-08-24 11:05:05 +0100

    added axios and api call

--- print-app/src/endpoints/api.js
+++ print-app/src/endpoints/api.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useQuery } from "react-query";
+const axios = require("axios");
 
 export function useQueryGetPrinters() {
   return useQuery("getPrinters", () =>
     fetch("http://localhost:3001/printers").then((res) => res.json())
+  );
+}
+
+export function useQueryPutLogin(username, password) {
+  return useQuery("putLogin", () =>
+    axios.put("http://localhost:3001/putLogin", { username: username, password: password}).then((res) => res.json)
   );
 }
 
