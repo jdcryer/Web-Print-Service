fb5f72eaf5319d3e22361af7be4afc115bb0612e
Samuel Guard <sg2268@bath.ac.uk>
2022-06-14 10:56:10 +0100

    removed header on generated barcode

--- service/build/modules/label_constructor.js
+++ service/build/modules/label_constructor.js
@@ -183,7 +183,7 @@ function buildBarcode(input, prop, dpi, units) {
       )
       .then((res) => {
         resolve(`<g transform="translate(${left}, ${top})">
-          ${res.data}
+          ${res.data.split("\n").slice(2).join("\n")}
         </g>`);
       })
       .catch(reject);
